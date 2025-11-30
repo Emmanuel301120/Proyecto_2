9d31df0b4c9321ad08e369ba8702bec576d9f85c
JuanIgnacioMoraLeiva <112134024+JuanIgnacioMoraLeiva@users.noreply.github.com>
2025-11-30 14:08:12 -0600

    Update Nodo.h

--- Nodo.h
+++ Nodo.h
@@ -1,18 +1,17 @@
 #ifndef NODO_H
 #define NODO_H
 #include <string>
-#include "arbolRB.h"
-#include "searchDatos.h"
 using namespace std;
 
 template <typename T>
 class Nodo{
     public:
-    //bool color = false; //Nodos negros = false Nodos rojos = true
+    bool color = false; //Nodos negros = false Nodos rojos = true
     T dato;
     string clave;
     Nodo<T>* left;
     Nodo<T>* right;
+    Nodo<T>* padre;
     
 
     Nodo(T& dato, string clave){

ce45765ac3d8229c979da418c12464c4873d98b8
JuanIgnacioMoraLeiva <112134024+JuanIgnacioMoraLeiva@users.noreply.github.com>
2025-12-03 13:35:14 -0600

    Update Vehiculo.h

--- Vehiculo.h
+++ Vehiculo.h
@@ -1,55 +1,28 @@
 #ifndef VEHICULO_H
 #define VEHICULO_H
+#include <string>
+#include <iostream>
+using namespace std;
+
 class Vehiculo{
     public:
     string color;
     int ruedas;
     int puertas;
 
-    Vehiculo(){
-        color = "Default";
-        ruedas = 0;
-        puertas = 0;
-    }
+    Vehiculo();
+    Vehiculo(string color, int ruedas, int puertas);
 
-
-
-    Vehiculo(string color, int ruedas, int puertas){
-        this->color = color;
-        this->ruedas = ruedas;
-        this->puertas = puertas;
-    }
-
-    bool operator<(const Vehiculo& v) const {
-        return this->color < v.color;
-    }
+    bool operator<(const Vehiculo& v) const;
     
-    bool operator==(const Vehiculo& v) const {
-        return this->color == v.color;
-    }
+    bool operator==(const Vehiculo& v) const;
 
-    bool operator!=(const Vehiculo& v) const {
-        return this->color != v.color;
-    }
+    bool operator!=(const Vehiculo& v) const;
 
+    bool operator>(const Vehiculo& v) const;
 
-    bool operator>(Vehiculo& v){
-        if(this->color>v.color){
-            return true;
-        }else{
-            return false;
-        }
-    }
+    string toString() const;
 
-    string toString(){
-        stringstream ss;
-        ss<<"Color: "<<color<<"-Ruedas: "<<ruedas<<"-Puertas: "<<puertas;
-        return ss.str();
-    }
-
-    friend ostream& operator<<(ostream& os, Vehiculo& car){
-        os<<car.toString();
-        return os;
-    }
+    friend ostream& operator<<(ostream& os, const Vehiculo& car);
 };
 #endif
